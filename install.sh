4720c8eb7ed89ee734707f8c6e34a58ad4c7e885
wh1t3P1g <wh1t3p1g@gmail.com>
2018-07-03 19:20:04 +0800

    version1.0

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt-get install -y docker-ce
+sudo apt-get install -y python-dev python-pip
+sudo pip install docker-compose
